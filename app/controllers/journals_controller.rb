cf9d4c8a0193d2c97d1e07b7f92625dd85beecd3
kacy hulme <kacyhulme@gmail.com>
2014-09-23 14:55:36 -0500

    add more templating and linking

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -4,22 +4,35 @@ class JournalsController < ApplicationController
   end
 
   def show
-    @journal = Journal.find(params[:journal_id])
+    @journal = Journal.find(params[:id])
   end
 
   def new
+    @journal = Journal.new
   end
 
   def create
+    @journal = Journal.new(journal_params)
+    if @journal.save
+      redirect_to @journal,
+      notice: "Your project was created"
+    else
+      render :new
+    end
   end
 
   def edit
+    @journal = Journal.find(params[:id])
   end
 
   def update
+    @journal = Journal.find(params[:id])
+    @journal.update(journal_params)
   end
 
   def destroy
+    @journal.destroy
+    redirect_to journals_url, alert: "Journal was deleted"
   end
 
   private
