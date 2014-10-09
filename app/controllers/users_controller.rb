040c3b7bf07c0cccb330efada5b3af879f921e45
kacy hulme <kacyhulme@gmail.com>
2014-10-09 11:45:33 -0500

    adds paths and uses views from devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+      @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user, notice: "Your account was created!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Your account has been updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_url, alert: "Your account was successfully deleted."
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_me)
+  end
 end
